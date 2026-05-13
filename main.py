0bc04750d045187ce983c71e72fa4afe73b5f027
otabekbotirov330-collab <otabekbotirov330@gmail.com>
2026-05-13 17:14:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram.types import FSInputFile
 
 # --- SOZLAMALAR ---
-TOKEN = "7919823792:AAHDzUwHNJt36NMjRWTaaHhvHgRewRHWWic"
+TOKEN = "7919823792:AAHGbtSQdf7qf0DQGBS-Wch5Nq1LWau75Fo"
 ADMIN_ID = 8323916383
 ADMIN_FOLDER = "admin_data"
 CERT_FOLDER = "certificates"
